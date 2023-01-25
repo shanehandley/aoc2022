849619b14b00f194a2d4cc87e56bf7e3e1916cbb
shane <shanehandley@fastmail.com>
2023-01-25 14:36:02 +1100

    WIP day 09

--- typescript/day09.ts
+++ typescript/day09.ts
@@ -1,64 +1,150 @@
 import { inspect } from 'util'
 import { loadInput } from './index'
 
+type Direction = 'R' | 'U' | 'L' | 'D'
+
 const process = async () => {
     const data = await loadInput('09')
     const lines = data.toString().split(/[\n]/)
 
-    const grid = [['s']]
+    const row = '.'.repeat(6).split('')
+
+    let grid = [
+        row,
+        row,
+        row,
+        row,
+        row,
+        row,
+        ['#', ...'.'.repeat(5).split('')]
+    ]
+
     const result: string[] = []
 
-    let yIdx = 0
+    let yIdx = grid.length - 1
     let xIdx = 0
 
-    let hPos = new Map<number, number[]>()
-    let tPos = new Map<number, number[]>()
+    let headPosition: [number, number] = [0, 0]
+    let tailPosition: [number, number] = [0, 0]
 
-    hPos.set(0, [])
+    const clearHead = () => grid = grid.map(row => row.map((cell) => cell === 'H' ? '.' : cell))
+
+    const setHeadPosition = (yIdx: number, xIdx: number) => {
+        headPosition = [yIdx, xIdx]
+
+        clearHead()
+
+        grid[yIdx][xIdx] = 'H'
+    }
+
+    const clearTail = () => grid = grid.map(row => row.map((cell) => cell === 'T' ? '#' : cell))
+
+    // If the tail is already adjacent to the head, there's no movement
+    const setTailPosition = (yIdx: number, xIdx: number, direction: Direction) => {
+        const [yTail, xTail] = tailPosition
+
+        // If the current head position is within 1 of us, we don't need to move
+        // if (Math.abs(yHead - yTail) <= 1) {
+        //     console.log('SKIPPING TAIL MOVEMENT', yHead, xHead)
+
+        //     return
+        // }
+
+        switch (direction) {
+            case 'R': {
+                for (let index = xIdx; index != 0; index--) {
+                    if (grid[yIdx][index] !== 'H') {
+                        grid[yIdx][index] = '#'
+                    }
+                }
+
+
+                break
+            }
+            case 'U':
+                for (let index = yIdx; index != 0; index--) {
+                    if (grid[index][xIdx] !== 'H') {
+                        grid[index][xIdx] = '#'
+                    }
+                }
+
+                break
+            case 'L':
+            case 'D': {
+                clearTail()
+                grid[yIdx][xIdx] = 'T'
+            }
+        }
+
+        grid[yIdx][xIdx] = 'T'
+        tailPosition = [yIdx, xIdx]
+    }
 
     for (const [idx, line] of lines.entries()) {
-        const [direction, _amount] = line.split(' ')
+        const [direction, _amount] = line.split(' ') as [Direction, string]
         const amount = parseInt(_amount, 10)
 
         switch (direction) {
             case 'R': {
-                xIdx =+ amount
+                xIdx = + amount
 
-                hPos.set(0, [yIdx, xIdx])
+                setHeadPosition(yIdx, xIdx)
+
+                if (amount > 1) {
+                    setTailPosition(yIdx, xIdx - 1, direction)
+                }
+
                 break
             }
             case 'U': {
-                yIdx += amount
+                // Check that we have the vertical space in our grid
+                for (let index = 1; index <= amount; index++) {
+                    if (grid[yIdx - index] === undefined) {
+                        grid[yIdx - index] = row
+                    }
+                }
 
-                hPos.set(0, [yIdx, xIdx])
+                yIdx -= amount
+
+                setHeadPosition(yIdx, xIdx)
+
+                if (amount > 1) {
+                    setTailPosition(yIdx + 1, xIdx, direction)
+                }
+
                 break
             }
             case 'L': {
                 xIdx -= amount
+
+                setHeadPosition(yIdx, xIdx)
+
+                if (amount > 1) {
+                    setTailPosition(yIdx, xIdx + 1, direction)
+                }
+
                 break
             }
             case 'D': {
                 yIdx -= amount
+
+                setHeadPosition(yIdx, xIdx)
+
+                if (amount > 1) {
+                    setTailPosition(yIdx + 1, xIdx, direction)
+                }
+
                 break
             }
             default: {
-                console.log('LINE', line)
                 throw new Error(`unhandled direction: ${direction}`)
-
             }
         }
 
         console.log(direction, amount)
-        console.log('HPOS', hPos)
+        console.table(grid)
 
-        // Move the head to that position
-        // grid[yIdx][xIdx] = 'H'
-
-        if (idx > 0) {
-            switch (direction) {
-
-            }
-        }
+        // break
     }
 
     return result
