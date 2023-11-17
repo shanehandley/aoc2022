ea96e3faf7fe6f0fb105598dec1c7ea2f1883774
Shane Handley <shanehandley@fastmail.com>
2023-11-17 16:58:19 +1100

    Use reduce for day2 part2 just for fun

--- ruby/day02/part2.rb
+++ ruby/day02/part2.rb
@@ -3,7 +3,7 @@
 file_path = File.expand_path('../../inputs/day02.txt', __dir__)
 input     = File.read(file_path)
 
-scores = input.each_line.map do |line|
+result = input.each_line.reduce(0) do |sum, line|
   play, outcome = line.split
 
   # the second column says how the round needs to end: X means you need to lose, Y means you need
@@ -20,14 +20,14 @@ scores = input.each_line.map do |line|
   # The index of the response is 0-based, but we are exploiting the fact that the score we get for the
   # selection of (rock,scissors,paper) is just (1) + (the chosen items index)
   response_score = 1 + if outcome_index == 0 # we need to lose, return the the choice that preeceeds theirs
-                     (response_index - 1) % 3
-                   elsif outcome_index == 1 # draw, return their choice
-                     response_index
-                   else # win, return their choice + 1
-                     (response_index + 1) % 3
-                   end
+                         (response_index - 1) % 3
+                       elsif outcome_index == 1 # draw, return their choice
+                         response_index
+                       else # win, return their choice + 1
+                         (response_index + 1) % 3
+                       end
 
-  outcome_score + response_score
+  sum + outcome_score + response_score
 end
 
-puts scores.sum
+puts result
---
+++ ruby/day03/part2.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+file_path = File.expand_path('../../inputs/day03.txt', __dir__)
+input     = File.read(file_path)
