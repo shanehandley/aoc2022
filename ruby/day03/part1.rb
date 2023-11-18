6c8b9d28d9f8570cb7e7f01dea035ebfa224a731
Shane Handley <shanehandley@fastmail.com>
2023-11-18 19:10:43 +1100

    Day 3 solution

--- ruby/day03/part1.rb
+++ ruby/day03/part1.rb
@@ -2,3 +2,26 @@
 
 file_path = File.expand_path('../../inputs/day03.txt', __dir__)
 input     = File.read(file_path)
+
+priorities = input.each_line.reduce(0) do |sum, line|
+  # split in half as two lists of chars
+  first, second = line.each_char.each_slice(line.length / 2).to_a
+
+  # Find the item type that appears in both compartments of each rucksack.
+  # array intersections: x & y - there is only ever a single item so `.first`
+  intersection = (first & second).first
+
+  # Lowercase item types a through z have priorities 1 through 26.
+  # Uppercase item types A through Z have priorities 27 through 52.
+
+  # we need to know whether its upper or lowercase
+  is_lowercase = intersection.downcase == intersection
+
+  # If it is lowercase, we can get the char ord and remove 96 ('a'.ord == 97)
+  # else we should remove 38 ('A'.ord == 65) to reset to 27
+  subtraction_factor = is_lowercase ? 96 : 38
+
+  sum + intersection.ord - subtraction_factor
+end
+
+puts priorities
--- ruby/day03/part2.rb
+++ ruby/day03/part2.rb
@@ -2,3 +2,21 @@
 
 file_path = File.expand_path('../../inputs/day03.txt', __dir__)
 input     = File.read(file_path)
+
+# From part one
+def get_priority(item)
+  is_lowercase = item.downcase == item
+
+  is_lowercase ? item.ord - 96 : item.ord - 38
+end
+
+priorities = input.each_line.each_slice(3).reduce(0) do |sum, lines|
+  # we are provided three lines, we want to find a common character in each line
+  # convert it to three lists of chars and find the intersection
+  first, second, third = lines.map(&:strip).map(&:chars)
+  intersection = first & second & third
+
+  sum + get_priority(intersection.first)
+end
+
+puts priorities
